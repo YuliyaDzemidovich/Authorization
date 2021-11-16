950b9a8be20cfb5f7c0fc9e1f6c22dc59085a34e
Yuliya Dzemidovich <yuliya.dzemidovich@gmail.com>
2021-11-17 02:00:04 +0300

    added one user for in-memory auth

---
+++ InMemoryUser/src/main/java/config/Config.java
@@ -0,0 +1,29 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import security.InMemoryUserDetailsService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails u = new User("yuliya", "12345", new ArrayList<>()); // empty list of authorities
+        List<UserDetails> users = List.of(u);
+        return new InMemoryUserDetailsService(users);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ InMemoryUser/src/main/java/security/InMemoryUserDetailsService.java
@@ -0,0 +1,23 @@
+package security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class InMemoryUserDetailsService implements UserDetailsService {
+
+    // UserDetailsService manages the list of users in-memory
+    private final List<UserDetails> users;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return users.stream()
+                .filter(u -> u.getUsername().equals(username))
+                .findFirst()
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
